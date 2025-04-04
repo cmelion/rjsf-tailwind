68a264c313838aca65dc383bff18670115d21e85
cmelion <charles.fulnecky@warnermedia.com>
2025-04-04 00:42:18 -0400

    Fix basepath used by Github Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// In src/main.tsx
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
@@ -5,28 +6,23 @@ import "./index.css"
 
 import { BrowserRouter as Router } from "react-router-dom"
 const alwaysDev = true;
-// Create an async bootstrap function to set up MSW
+
 async function bootstrap() {
-  // Only initialize MSW in development mode
-  // if (process.env.NODE_ENV === 'development') {
   if (alwaysDev) {
-    // Dynamically import the MSW browser module
     const { worker } = await import('./mocks/browser')
-    // Start the worker with configuration
     await worker.start({
       onUnhandledRequest: 'bypass',
     })
     console.log('MSW initialized')
   }
-  // Render the React app after MSW is ready (if applicable)
+
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <App />
       </Router>
     </React.StrictMode>
   )
 }
 
-// Call the bootstrap function to start the app
 bootstrap()

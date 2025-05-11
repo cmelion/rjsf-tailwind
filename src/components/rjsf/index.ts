5f16c2089335a5546fe491afecf9ff200491a5a2
cmelion <cfulnecky@gmail.com>
2025-05-11 16:59:24 -0400

    Refactor index exports in Form component; change IChangeEvent export to type export

--- src/components/rjsf/index.ts
+++ src/components/rjsf/index.ts
@@ -1,6 +1,6 @@
 import Form from "@/components/rjsf/form/"
 export { default as Form } from "./form/"
-export { generateForm, IChangeEvent } from "./form/"
+export type { IChangeEvent } from "./form/"
 export { generateTemplates } from "./templates"
 export { generateTheme } from "./theme"
 export { default as Widgets, generateWidgets } from "./widgets"
